bb082655519a6530e919ad6a6edd3ed3f4821b4a
shiqi <1475651660@qq.com>
2018-08-15 23:53:41 +0800

    config

---
+++ CONFIG.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+URL_FOR_SEARCH = "https://shc.y.qq.com/soso/fcgi-bin/client_search_cp?"
+
+URL_FOR_SONGMID = "https://c.y.qq.com/v8/fcg-bin/fcg_v8_album_info_cp.fcg?"
+
+URL_FOR_VKEY = "https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg?"
+
+URL_FOR_VIPSONG = "http://dl.stream.qqmusic.qq.com/C400000Md1wq0vnwzE.m4a?"
